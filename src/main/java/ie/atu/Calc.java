56da2e6781102fba5175b475209278a00516a1ba
PATRICKFLANAGAN-STUD <G00431255@atu.ie>
2024-09-30 09:58:07 +0100

    Fixed Feature 1

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,4 +1,40 @@
 package ie.atu;
 
+import java.util.Scanner;
 public class Calc {
+    public static void main(String[] args) {
+
+        Scanner sc1= new Scanner(System.in);
+
+        System.out.println("Please enter a number");
+        double firstNum = sc1.nextDouble();
+        System.out.println("You entered " + firstNum);
+
+
+        System.out.println("Please enter a second number");
+        double secondNum = sc1.nextDouble();
+        System.out.println("You entered " + secondNum);
+
+        System.out.println("choose a operator: (add,sub,mul,div)");
+        String operator = sc1.next();
+        switch(operator){
+            case"add":
+                add(firstNum,secondNum);
+                break;
+            default:
+                System.out.println("Invalid operation");
+                break;
+
+        }
+
+
+    }
+
+
+    public static void add(double first,double second) {
+        double result = first + second;
+        System.out.println("the total is " + result);
+
+
+    }
 }
